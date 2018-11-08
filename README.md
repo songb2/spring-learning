064c7df9feecb94fc6fda9a3079520c69cd40a5b
Bruce Song <BruceSong2010@hotmail.com>
2018-11-08 02:08:55 -0800

    remove unecessary files

--- README.md
+++
@@ -1,2 +0,0 @@
-# spring-learning
-learn spring by practicing
--- microservice-simple-provider-user/target/classes/data.sql
+++
@@ -1,4 +0,0 @@
-insert into user (id, username, name, age, balance) values (1, 'account1', 'Tom', 12, 100.0);
-insert into user (id, username, name, age, balance) values (2, 'account2', 'Jerry', 23, 200.0);
-insert into user (id, username, name, age, balance) values (3, 'account3', 'Reno', 44, 300.0);
-insert into user (id, username, name, age, balance) values (4, 'account4', 'Josh', 55, 400.0);
