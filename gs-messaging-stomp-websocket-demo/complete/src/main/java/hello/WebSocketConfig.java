97d84637dc56cbdda4bf103facf59b81c99ad86a
Bruce Song <BruceSong2010@hotmail.com>
2018-11-08 20:53:34 -0800

    add comments

--- gs-messaging-stomp-websocket-demo/complete/src/main/java/hello/WebSocketConfig.java
+++ gs-messaging-stomp-websocket-demo/complete/src/main/java/hello/WebSocketConfig.java
@@ -6,16 +6,17 @@ import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBr
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
-@Configuration
-@EnableWebSocketMessageBroker
+@Configuration // spring configuration class
+@EnableWebSocketMessageBroker // enable WebSocket message handling
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic");
-        config.setApplicationDestinationPrefixes("/app");
+        config.enableSimpleBroker("/topic"); // a simple memory-based broker
+        config.setApplicationDestinationPrefixes("/app"); // endpoint like /app/hello
     }
 
+    // enable SockJS fallback options (websocket, xhr-streaming, xhr-polling)
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/gs-guide-websocket").withSockJS();
--- gs-messaging-stomp-websocket-demo/complete/src/main/resources/static/app.js
+++ gs-messaging-stomp-websocket-demo/complete/src/main/resources/static/app.js
@@ -13,11 +13,12 @@ function setConnected(connected) {
 }
 
 function connect() {
-    var socket = new SockJS('/gs-guide-websocket');
+    var socket = new SockJS('/gs-guide-websocket'); // socket server
     stompClient = Stomp.over(socket);
     stompClient.connect({}, function (frame) {
         setConnected(true);
         console.log('Connected: ' + frame);
+        // where the server will publish greeting messages
         stompClient.subscribe('/topic/greetings', function (greeting) {
             showGreeting(JSON.parse(greeting.body).content);
         });
