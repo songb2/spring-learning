1a0b425e03ae707a6b9e5b1cf5c4f3d27d1cc96d
Bruce Song <songpengpeng@songpengpengs-MacBook-Pro.local>
2018-11-09 21:40:47 +0800

    add comments

--- gs-scheduling-tasks/complete/src/main/java/hello/Application.java
+++ gs-scheduling-tasks/complete/src/main/java/hello/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
+@EnableScheduling // ensures a background task executor is created
 public class Application {
 
     public static void main(String[] args) {
